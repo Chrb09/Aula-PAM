30f4ed9568a004d00d692e44415ee386f416ab69
Chrb09 <carloshrbarile@gmail.com>
2024-04-19 16:34:38 -0300

    update

--- app/src/main/java/com/example/aulapam/MainActivity.kt
+++ app/src/main/java/com/example/aulapam/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -17,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.aulapam.ui.theme.AulaPAMTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,41 +43,87 @@ class MainActivity : ComponentActivity() {
 fun App(){
     Column(
         Modifier
-            .background(Color.Black)
+            .background(Color.White)
             .fillMaxWidth()
     ) {
         Row(
             Modifier
-                .background(Color.White)
                 .fillMaxWidth(),
             Arrangement.Center
         ) {
             Text(text = "App de notícias")
         }
+        Row(
+            Modifier
+                .padding(8.dp)
+        ) {
 
-        Row(
-            Modifier
-                .background(Color.White)
-                .fillMaxWidth(),
-        ) {
-            Text(text = "Titulo da notícia")
         }
-        Row(
+
+        Column(
             Modifier
-                .background(Color.White)
-                .fillMaxWidth(),
+                .background(Color.Gray)
         ) {
-            Text(text = "Descrição curta da notícia")
-        }
-        Row(
-            Modifier
-                .background(Color.White)
-                .fillMaxWidth(),
-            Arrangement.Absolute.Right
-        ) {
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Leia Mais")
+            Row(
+                Modifier
+                    .fillMaxWidth(),
+            ) {
+                Text(text = "Titulo da notícia")
             }
+
+            Row(
+                Modifier
+                    .fillMaxWidth(),
+            ) {
+                Text(text = "Descrição curta da notícia")
+            }
+            Row(
+                Modifier
+                    .fillMaxWidth(),
+                Arrangement.Absolute.Right
+            ) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Leia Mais")
+                }
+            }
+        }
+        Row(
+            Modifier
+                .padding(8.dp)
+        ) {
+
+        }
+        Column(
+            Modifier
+                .background(Color.Gray)
+        ) {
+            Row(
+                Modifier
+                    .fillMaxWidth(),
+            ) {
+                Text(text = "Titulo da notícia")
+            }
+            Row(
+                Modifier
+                    .fillMaxWidth(),
+            ) {
+                Text(text = "Descrição curta da notícia")
+            }
+            Row(
+                Modifier
+                    .fillMaxWidth(),
+                Arrangement.Absolute.Right
+            ) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Leia Mais")
+                }
+            }
+        }
+        Row(
+            Modifier
+                .padding(8.dp)
+        ) {
+
         }
     }
 }
